75447f48c9f7027b48531a594df61b49bc9c67fe
ben522887 <114271002+ben522887@users.noreply.github.com>
2022-10-07 23:26:45 +0800

    Add files via upload

--- selectbi.js
+++ selectbi.js
@@ -25,9 +25,10 @@ $.ajax({
     datatype: JSON
 })
 let addinformation = function(data){
+    sessionStorage.setItem(`${symbolID}`,data);
     $("#h1").text(`${data[0]["symbolId"]}-${data[0]["comname"]}`);
-    $("#td1").text(data[0]["comname"]);
-    $("#td2").text(data[0]["engadd"]);
+    $("#td1").text(data[0]["comName"]);
+    $("#td2").text(data[0]["engAdd"]);
     $("#td3").text(data[0]["estab"]); 
     $("#td4").text(data[0]["ld"]);
     $("#td5").text(data[0]["lc"]);
@@ -35,18 +36,18 @@ let addinformation = function(data){
     $("#td7").text(data[0]["gm"]);
     $("#td8").text(data[0]["sc"]);
     $("#td9").text(data[0]["nooso"]);
-    $("#td10").text(data[0]["mcM"]);
+    $("#td10").text(data[0]["mcm"]);
     $("#td11").text(data[0]["srodas"]);
     $("#td12").text(data[0]["mb"]);
     $("#td13").text(data[0]["spokesman"]);
     $("#td14").text(data[0]["actsp"]);
     $("#td15").text(data[0]["tephsw"]);
     $("#td16").text(data[0]["fxn"]);
-    $("#td17").text(data[0]["cwebsite"]);
+    $("#td17").attr(href,data[0]["cWebsite"]).text(data[0]["cwebsite"]);
     $("#td18").text(data[0]["email"]);
     $("#td19").text(data[0]["sa"]);
     $("#td20").text(data[0]["va"]);
-    $("#td21").text(data[0]["comadd"]);
+    $("#td21").text(data[0]["comAdd"]);
     $("#td22").text(data[0]["market"]);
     $("#td23").text(data[0]["grp"]);
 }
